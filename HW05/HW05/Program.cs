83395c7413ae0b14cf6e301dd88494ddf07e43c6
ChaoticEvan <evan.voordeckers@yahoo.com>
2021-11-30 13:09:53 -0700

    Added experiments

--- HW05/HW05/Program.cs
+++ HW05/HW05/Program.cs
@@ -8,12 +8,67 @@ namespace HW05
 {
     class Program
     {
-        private const float aProb = 0.52f;
-        private const float bProb = 0.48f;
+        private const int prob = 52;
 
         static void Main(string[] args)
         {
+            Console.WriteLine("Running experiment A (20 runs)");
+            Results expAResult = RunExperiment(20);
+            Console.WriteLine("Done...");
 
+            Console.WriteLine("Running experiment B (100 runs)");
+            Results expBResult = RunExperiment(100);
+            Console.WriteLine("Done...");
+
+            Console.WriteLine("Running experiment C (400 runs)");
+            Results expCResult = RunExperiment(400);
+            Console.WriteLine("Done...");
+
+            Console.ReadLine();
         }
+
+        private static Results RunExperiment(int numRuns)
+        {
+            Random rand = new Random();
+            int aCount = 0, bCount = 0;
+
+            for (int i = 0; i < numRuns; ++i)
+            {
+                int voter = rand.Next(100);
+                bool didVoteA = voter <= prob;
+
+                if (didVoteA)
+                {
+                    aCount++;
+                }
+                else
+                {
+                    bCount++;
+                }
+            }
+
+            Results res;
+            if (aCount > bCount)
+            {
+                res = Results.AMajority;
+            }
+            else if (bCount > aCount)
+            {
+                res = Results.BMajority;
+            }
+            else
+            {
+                res = Results.Tie;
+            }
+
+            return res;
+        }
+    }
+
+    enum Results
+    {
+        AMajority,
+        BMajority,
+        Tie
     }
 }
