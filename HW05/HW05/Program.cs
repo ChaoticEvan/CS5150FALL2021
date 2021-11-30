3f0a53517720594d29ac4e8bdc92b8b359cf75bf
ChaoticEvan <evan.voordeckers@yahoo.com>
2021-11-30 12:53:23 -0700

    init project commit

---
+++ HW05/HW05/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW05
+{
+    class Program
+    {
+        private const float aProb = 0.52f;
+        private const float bProb = 0.48f;
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
