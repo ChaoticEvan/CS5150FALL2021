5fba38f66e5841e393e205410f676aea23d567b2
Evan Voordeckers <evan.voordeckers@yahoo.com>
2021-12-06 18:27:29 -0700

    Writing output to file

--- HW06/HW06/HW06/Program.cs
+++ HW06/HW06/HW06/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace HW06
 {
     class Program
     {
-        static void Main(string[] args)
+        static Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> bins;
+            bins = new List<int>(new int[10_000_000]);            
+
+            Random rand = new Random();
+            for(int i = 0; i < 10_000_000; ++i)
+            {
+                int idx = rand.Next(10_000_000);
+                bins[idx]++;
+            }
+
+            File.WriteAllText("C:\\Users\\evanv\\Desktop\\HW6Q2Output.txt", "Bin #    Balls");
+
+            string[] output = new string[10_000_000];
+            for (int i = 0; i < 10_000_000; ++i)
+            {
+                output[i] = i + "    " + bins[i];
+            }
+
+            File.WriteAllLines("C:\\Users\\evanv\\Desktop\\HW6Q2Output.txt", output);
         }
     }
 }
