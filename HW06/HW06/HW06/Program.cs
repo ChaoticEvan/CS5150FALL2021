ae1a2982afb57827bd895d2217a7650d3023eeae
Evan Voordeckers <evan.voordeckers@yahoo.com>
2021-12-06 18:55:33 -0700

    Edited to use two indexes and report distrubution

--- HW06/HW06/HW06/Program.cs
+++ HW06/HW06/HW06/Program.cs
@@ -7,24 +7,31 @@ namespace HW06
 {
     class Program
     {
-        static Task Main(string[] args)
+        static void Main(string[] args)
         {
             List<int> bins;
-            bins = new List<int>(new int[10_000_000]);            
+            bins = new List<int>(new int[10_000]);            
 
             Random rand = new Random();
-            for(int i = 0; i < 10_000_000; ++i)
+            for(int i = 0; i < 10_000; ++i)
             {
-                int idx = rand.Next(10_000_000);
-                bins[idx]++;
+                int idx1 = rand.Next(10_000);
+                int idx2 = rand.Next(10_000);
+                if(bins[idx1] < bins[idx2])
+                {
+                    bins[idx1]++;
+                }
+                else
+                {
+                    bins[idx2]++;
+                }
             }
 
-            File.WriteAllText("C:\\Users\\evanv\\Desktop\\HW6Q2Output.txt", "Bin #    Balls");
-
-            string[] output = new string[10_000_000];
-            for (int i = 0; i < 10_000_000; ++i)
+            string[] output = new string[10_001];
+            output[0] = "Bin #    Balls";
+            for (int i = 0; i < 10_000; ++i)
             {
-                output[i] = i + "    " + bins[i];
+                output[i+1] = i + "~~~~" + bins[i];
             }
 
             File.WriteAllLines("C:\\Users\\evanv\\Desktop\\HW6Q2Output.txt", output);
